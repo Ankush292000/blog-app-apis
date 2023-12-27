75667dc90773768a9de71349105e2e66a1bbce5a
Ankush292000 <86279943+Ankush292000@users.noreply.github.com>
2023-12-27 17:20:11 +0530

    Validation in API
    validate added email and user name validation

--- blog-app-apis/src/main/java/com/API/blog/payloads/UserDto.java
+++ blog-app-apis/src/main/java/com/API/blog/payloads/UserDto.java
@@ -1,5 +1,9 @@
 package com.API.blog.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,11 +16,16 @@ public class UserDto {
 	
 	private int id;
 	
+	@NotEmpty
+	@Size(min =4 ,message = "username must be 4 characters")
 	private String name;
 	
+	@Email
 	private String email;
 	
+	@NotEmpty
 	private String password;
 	
+	@NotNull
 	private String about;
 }
