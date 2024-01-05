cdada9fbeb7a69e9ac66fd554e6a8476b3a38a95
Ankush292000 <86279943+Ankush292000@users.noreply.github.com>
2024-01-05 17:00:34 +0530

    pagging
    All operations of paging in post API

--- blog-app-apis/src/main/java/com/API/blog/controllers/PostController.java
+++ blog-app-apis/src/main/java/com/API/blog/controllers/PostController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.API.blog.entities.Post;
 import com.API.blog.payloads.ApiResponse;
 import com.API.blog.payloads.PostDto;
+import com.API.blog.payloads.PostResponse;
 import com.API.blog.services.PostService;
 
 @RestController
@@ -57,9 +59,14 @@ public class PostController {
 		}
 		//get All post
 		@GetMapping("/posts")
-		public ResponseEntity<List<PostDto>> getAllPost(){
-			List<PostDto> allpost = this.postService.getAllPost();
-			return new ResponseEntity<List<PostDto>>(allpost , HttpStatus.OK);
+		public ResponseEntity<PostResponse> getAllPost(
+				@RequestParam(value = "pageNumber" , defaultValue ="0" , required = false) Integer pageNumber ,
+				@RequestParam(value = "pageSize" , defaultValue ="6" , required = false) Integer pageSize,
+				@RequestParam(value = "sortBy" , defaultValue = "postId" ,required = false) String sortBy,
+				@RequestParam(value = "sortDir" , defaultValue = "asc" , required =  false) String sortDir
+				){
+			PostResponse allpost = this.postService.getAllPost(pageNumber , pageSize,sortBy,sortDir);
+			return new ResponseEntity<PostResponse>(allpost , HttpStatus.OK);
 		}
 		
 		//get by ID
